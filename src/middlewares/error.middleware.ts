c4105e6fec3f4b107bee336de09657a71013da2e
Carlito Patricio <carlpatricio23@gmail.com>
2024-06-07 06:52:55 +0800

    test(middlewares): added unit testing

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -4,39 +4,8 @@ import {
   Request,
   Response,
 } from 'express';
-import { ErrorRequestHandler } from 'express-serve-static-core'; // Interface for ErrorRequestHandler
 import { logger } from '../common';
 
-export const errorMiddleware: ErrorRequestHandler =
-  (
-    err,
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    const { status, message: errMsg } =
-      err;
-    let message = errMsg;
-    if (err instanceof AxiosError) {
-      message =
-        err.response?.data?.message ??
-        message;
-    }
-    console.log({ err });
-    logger.error(
-      JSON.stringify({
-        err,
-        message,
-        status,
-      })
-    );
-    res.status(status || 500).json({
-      status,
-      message,
-      timestamp: new Date(),
-    });
-    next();
-  };
 export class ErrorMiddleware {
   /**
    * Handles the error that occurred during the request.
@@ -53,8 +22,10 @@ export class ErrorMiddleware {
     res: Response,
     next: NextFunction
   ): void {
-    const { status, message: errMsg } =
-      err;
+    const {
+      status: code,
+      message: errMsg,
+    } = err;
     let message = errMsg;
     if (err instanceof AxiosError) {
       message =
@@ -65,10 +36,11 @@ export class ErrorMiddleware {
       JSON.stringify({
         err,
         message,
-        status,
+        code,
       })
     );
-    res.status(status || 500).json({
+    const status = code || 500;
+    res.status(status).json({
       status,
       message,
       timestamp: new Date(),
